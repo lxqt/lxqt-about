9a65bbce340f29c96ca44381b35eb826c70021df
Alf Gaida <agaida@siduction.org>
2015-06-20 13:00:01 +0200

    new licensing proposal
    new license headers
    rename COPYING to LICENSE
    new AUTHORS  w/o translators for now

--- aboutdialog/lxqtaboutdialog_p.h
+++ aboutdialog/lxqtaboutdialog_p.h
@@ -1,29 +1,11 @@
-/* BEGIN_COMMON_COPYRIGHT_HEADER
- * (c)LGPL2+
+/*
+ * Copyright (c) LXQt contributors.
  *
- * LXQt - a lightweight, Qt based, desktop toolset
- * http://razor-qt.org
- *
- * Copyright: 2010-2011 Razor team
- * Authors:
- *   Petr Vanek <petr@scribus.info>
- *
- * This program or library is free software; you can redistribute it
- * and/or modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
-
- * You should have received a copy of the GNU Lesser General
- * Public License along with this library; if not, write to the
- * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA 02110-1301 USA
- *
- * END_COMMON_COPYRIGHT_HEADER */
+ * This file is part of the LXQt project. <http://lxqt.org>
+ * It is distributed under the LGPL 2.1 or later license.
+ * Please refer to the LICENSE file for a copy of the license, and
+ * the AUTHORS file for copyright and authorship information.
+ */
 
 #ifndef LXQT_ABOUTDIALOG_P_H
 #define LXQT_ABOUTDIALOG_P_H
