72cc8d245944b4148f8dcfb0be1a245fc4a1f9f4
Thomas Viehweger <ThomasVie@users.noreply.github.com>
2015-06-20 22:39:04 +0200

    Removed obsolete contexts from liblxqt.

--- translations/lxqt-about_ja.ts
+++ translations/lxqt-about_ja.ts
@@ -18,7 +18,7 @@
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="98"/>
         <source>LXQt would not have been possible without the &lt;a %1&gt;Razor-qt&lt;/a&gt; project and its many contributors.</source>
         <comment>About dialog, &apos;About&apos; tab text</comment>
-        <translation>LxQtは、&lt;a %1&gt;Razor-qt&lt;/a&gt;プロジェクトとそのたくさんの貢献者たちなしにはありえませんでした。</translation>
+        <translation>LXQtは、&lt;a %1&gt;Razor-qt&lt;/a&gt;プロジェクトとそのたくさんの貢献者たちなしにはありえませんでした。</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="100"/>
@@ -64,185 +64,11 @@
     </message>
 </context>
 <context>
-    <name>AddPluginDialog</name>
-    <message>
-        <source>Search:</source>
-        <translation type="vanished">検索:</translation>
-    </message>
-    <message>
-        <source>Add Widget</source>
-        <translation type="vanished">ウィジェットを追加</translation>
-    </message>
-    <message>
-        <source>Close</source>
-        <translation type="vanished">閉じる</translation>
-    </message>
-    <message>
-        <source>(%1 active)</source>
-        <translation type="vanished">(%1 有効)</translation>
-    </message>
-</context>
-<context>
-    <name>ConfigDialog</name>
-    <message>
-        <source>Dialog</source>
-        <translation type="vanished">ダイアログ</translation>
-    </message>
-</context>
-<context>
-    <name>MessageBox</name>
-    <message>
-        <source>LxQt Power Manager Error</source>
-        <translation type="vanished">LxQt電源管理エラー</translation>
-    </message>
-    <message>
-        <source>Hibernate failed.</source>
-        <translation type="vanished">休止の失敗</translation>
-    </message>
-</context>
-<context>
-    <name>NotificationPrivate</name>
-    <message>
-        <source>Notifications Fallback</source>
-        <translation type="vanished">通知のフォールバック</translation>
-    </message>
-</context>
-<context>
-    <name>PowerManager</name>
-    <message>
-        <source>Hibernate</source>
-        <translation type="vanished">休止</translation>
-    </message>
-    <message>
-        <source>Suspend</source>
-        <translation type="vanished">サスペンド</translation>
-    </message>
-    <message>
-        <source>Reboot</source>
-        <translation type="vanished">再起動</translation>
-    </message>
-    <message>
-        <source>Shutdown</source>
-        <translation type="vanished">シャットダウン</translation>
-    </message>
-    <message>
-        <source>Logout</source>
-        <translation type="vanished">ログアウト</translation>
-    </message>
-    <message>
-        <source>LxQt Session Suspend</source>
-        <translation type="vanished">LxQtセッションのサスペンド</translation>
-    </message>
-    <message>
-        <source>Do you want to really suspend your computer?&lt;p&gt;Suspends the computer into a low power state. System state is not preserved if the power is lost.</source>
-        <translation type="vanished">本当にコンピュータをサスペンドしますか？&lt;P&gt;コンピュータを低消費電力状態にサスペンドします。電源が失われた場合にはシステムの状態は保持されません。</translation>
-    </message>
-    <message>
-        <source>LxQt Session Hibernate</source>
-        <translation type="vanished">LxQtセッションの休止</translation>
-    </message>
-    <message>
-        <source>Do you want to really hibernate your computer?&lt;p&gt;Hibernates the computer into a low power state. System state is preserved if the power is lost.</source>
-        <translation type="vanished">本当にコンピュータを休止状態にしますか？&lt;P&gt;コンピュータを
-低消費電力状態で休止します。電源が失われた場合でもシステムの状態は保持されます。</translation>
-    </message>
-    <message>
-        <source>LxQt Session Reboot</source>
-        <translation type="vanished">LxQtセッションの再起動</translation>
-    </message>
-    <message>
-        <source>Do you want to really restart your computer? All unsaved work will be lost...</source>
-        <translation type="vanished">本当にコンピュータを再起動しますか？保存されていない作業はすべて失われます...</translation>
-    </message>
-    <message>
-        <source>LxQt Session Shutdown</source>
-        <translation type="vanished">LxQtセッションのシャットダウン</translation>
-    </message>
-    <message>
-        <source>Do you want to really switch off your computer? All unsaved work will be lost...</source>
-        <translation type="vanished">本当にコンピュータをオフにしたいですか？保存されていないすべての作業は失われます...</translation>
-    </message>
-    <message>
-        <source>LxQt Session Logout</source>
-        <translation type="vanished">LxQtセッションのログアウト</translation>
-    </message>
-    <message>
-        <source>Do you want to really logout? All unsaved work will be lost...</source>
-        <translation type="vanished">本当にログアウトしたいですか？保存されていないすべての作業は失われます...</translation>
-    </message>
-    <message>
-        <source>LxQt Power Manager Error</source>
-        <translation type="vanished">LxQt電源管理エラー</translation>
-    </message>
-    <message>
-        <source>Hibernate failed.</source>
-        <translation type="vanished">休止の失敗</translation>
-    </message>
-    <message>
-        <source>Suspend failed.</source>
-        <translation type="vanished">サスペンドの失敗</translation>
-    </message>
-</context>
-<context>
-    <name>QObject</name>
-    <message>
-        <source>Power Manager Error</source>
-        <translation type="vanished">電源管理エラー</translation>
-    </message>
-    <message>
-        <source>QDBusInterface is invalid</source>
-        <translation type="vanished">QDbusInterfaceが無効です。</translation>
-    </message>
-    <message>
-        <source>Power Manager Error (D-BUS call)</source>
-        <translation type="vanished">電源マネージャエラー(D-BUSコール)</translation>
-    </message>
-</context>
-<context>
-    <name>ScreenSaver</name>
-    <message>
-        <source>Lock Screen</source>
-        <translation type="vanished">スクリーンをロック</translation>
-    </message>
-    <message>
-        <source>Screen Saver Activation Error</source>
-        <translation type="vanished">スクリーンセーバー起動エラー</translation>
-    </message>
-    <message>
-        <source>An error occurred starting screensaver. xdg-screensaver cannot be started due its crash.</source>
-        <translation type="vanished">スクリーンセーバーの起動時にエラーが発生しました。クラッシュが原因でxdg-スクリーンセーバーを起動できません。</translation>
-    </message>
-    <message>
-        <source>An error occurred starting screensaver. xdg-screensaver is not installed correctly.</source>
-        <translation type="vanished">スクリーンセーバーの起動時にエラーが発生しました。xdg-スクリーンセーバーが正しくインストールされていません。</translation>
-    </message>
-    <message>
-        <source>An error occurred starting screensaver. xdg-screensaver cannot be started.</source>
-        <translation type="vanished">スクリーンセーバーの起動時にエラーが発生しました。xdg-スクリーンセーバーを起動できません。</translation>
-    </message>
-    <message>
-        <source>An error occurred starting screensaver. Syntax error in xdg-screensaver arguments.</source>
-        <translation type="vanished">スクリーンセーバーの起動時にエラーが発生しました。xdg-スクリーンセーバーの引数に構文エラーがあります。</translation>
-    </message>
-    <message>
-        <source>An error occurred starting screensaver. Ensure you have xscreensaver installed and running.</source>
-        <translation type="vanished">スクリーンセーバーの起動時にエラーが発生しました。xscreensaverのインストールと実行を確認してください。</translation>
-    </message>
-    <message>
-        <source>An error occurred starting screensaver. Action &apos;activate&apos; failed. Ensure you have xscreensaver installed and running.</source>
-        <translation type="vanished">スクリーンセーバーの起動時にエラーが発生しました。&apos;Activate&apos;アクションが失敗しました。xscreensaverのインストールと実行を確認してください。</translation>
-    </message>
-    <message>
-        <source>An error occurred starting screensaver. Unknown error - undocumented return value from xdg-screensaver=%1.</source>
-        <translation type="vanished">スクリーンセーバーを開始する際にエラーが発生しました。不明なエラー - 文書化されていない返り値 xdg-screensaver=%1</translation>
-    </message>
-</context>
-<context>
     <name>about</name>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.ui" line="14"/>
         <source> About LXQt</source>
-        <translation>LxQtについて</translation>
+        <translation>LXQtについて</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.ui" line="66"/>
