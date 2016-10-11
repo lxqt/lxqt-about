830de221125f58f7c0d23987e4f1011d2b0c39a2
Sérgio Marques <smarquespt@gmail.com>
2016-10-11 23:34:07 +0100

    Update Portugese translations
    Some fixes, update locale in files and update obconf-qt to 100% (Portuguese)

--- lxqt-about_pt.ts
+++ lxqt-about_pt.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="pt">
+<TS version="2.1" language="pt">
 <context>
     <name>AboutDialogPrivate</name>
     <message>
@@ -12,7 +12,7 @@
         <location filename="../../aboutdialog/lxqtaboutdialog.cpp" line="96"/>
         <source>Advanced, easy-to-use, and fast desktop environment based on Qt technologies.</source>
         <comment>About dialog, &apos;About&apos; tab text</comment>
-        <translation>Ambiente de trabalho rápido, simples e fácil de utilizar baseado na tecnologia Qt.</translation>
+        <translation>Ambiente de trabalho rápido simples e fácil de utilizar, baseado na tecnologia Qt.</translation>
     </message>
     <message>
         <location filename="../../aboutdialog/lxqtaboutdialog.cpp" line="98"/>
