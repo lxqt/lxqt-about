e7169587039e2fa93ccd6372179fcd7a454f2ac9
ayhanyalcinsoy <ayhanyalcinsoy@pisilinux.org>
2016-12-21 00:07:49 +0200

    Add/update Turkish translations

--- lxqt-about_tr.ts
+++ lxqt-about_tr.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../../aboutdialog/lxqtaboutdialog.cpp" line="83"/>
         <source>Version: %1</source>
-        <translation>Versiyon: %1</translation>
+        <translation>Sürüm: %1</translation>
     </message>
     <message>
         <location filename="../../aboutdialog/lxqtaboutdialog.cpp" line="96"/>
