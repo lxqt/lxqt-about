20965fb85b89b97d6419068e704dd0ed1d55741b
Alf Gaida <agaida@siduction.org>
2018-06-03 18:01:47 +0200

    Translation update of lxqt-about

--- translations/lxqt-about_uk.ts
+++ translations/lxqt-about_uk.ts
@@ -4,178 +4,104 @@
 <context>
     <name>AboutDialogPrivate</name>
     <message>
-        <location filename="../../aboutdialog/lxqtaboutdialog.cpp" line="83"/>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="83"/>
         <source>Version: %1</source>
         <translation>Версія: %1</translation>
     </message>
     <message>
-        <location filename="../../aboutdialog/lxqtaboutdialog.cpp" line="96"/>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="96"/>
         <source>Advanced, easy-to-use, and fast desktop environment based on Qt technologies.</source>
         <comment>About dialog, &apos;About&apos; tab text</comment>
         <translation>Передове, легке у використанні та швидке стільникове середовище засноване на технологіях Qt.</translation>
     </message>
     <message>
-        <location filename="../../aboutdialog/lxqtaboutdialog.cpp" line="98"/>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="98"/>
         <source>LXQt would not have been possible without the &lt;a %1&gt;Razor-qt&lt;/a&gt; project and its many contributors.</source>
         <comment>About dialog, &apos;About&apos; tab text</comment>
         <translation>LXQt не було б можливим без проекту &lt;a %1&gt;Razor-qt&lt;/a&gt; і розробників, які до нього долучилися.</translation>
     </message>
     <message>
-        <source>Copyright: %1-%2 %3</source>
-        <comment>About dialog, &apos;About&apos; tab text</comment>
-        <translation type="vanished">Авторське право: %1-%2 %3</translation>
-    </message>
-    <message>
-        <location filename="../../aboutdialog/lxqtaboutdialog.cpp" line="100"/>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="100"/>
         <source>Copyright: © %1-%2 %3</source>
         <comment>About dialog, &apos;About&apos; tab text</comment>
         <translation>Всі права застережено: © %1-%2 %3 </translation>
     </message>
     <message>
-        <location filename="../../aboutdialog/lxqtaboutdialog.cpp" line="102"/>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="102"/>
         <source>Homepage: %1</source>
         <comment>About dialog, &apos;About&apos; tab text</comment>
         <translation>Домашня сторінка: %1</translation>
     </message>
     <message>
-        <location filename="../../aboutdialog/lxqtaboutdialog.cpp" line="104"/>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="104"/>
         <source>License: %1</source>
         <comment>About dialog, &apos;About&apos; tab text</comment>
         <translation>Ліцензія: %1</translation>
     </message>
     <message>
-        <location filename="../../aboutdialog/lxqtaboutdialog.cpp" line="114"/>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="114"/>
         <source>LXQt is developed by the &lt;a %1&gt;LXQt Team and contributors&lt;/a&gt;.</source>
         <comment>About dialog, &apos;Authors&apos; tab text</comment>
         <translation>LXQt розроблене &lt;a %1&gt;Командою LXQt Team і її помічниками&lt;/a&gt;.</translation>
     </message>
     <message>
-        <location filename="../../aboutdialog/lxqtaboutdialog.cpp" line="116"/>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="116"/>
         <source>If you are interested in working with our development team, &lt;a %1&gt;join us&lt;/a&gt;.</source>
         <comment>About dialog, &apos;Authors&apos; tab text</comment>
         <translation>Якщо Ви зацікавлені в роботі з нашою командою розробників, тоді &lt;a %1&gt;долучайтесь до нас&lt;/a&gt;.</translation>
     </message>
     <message>
-        <location filename="../../aboutdialog/lxqtaboutdialog.cpp" line="133"/>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="133"/>
         <source>Special thanks to:</source>
         <comment>About dialog, &apos;Thanks&apos; tab text</comment>
         <translation>Особливі подяки для:</translation>
     </message>
     <message>
-        <location filename="../../aboutdialog/lxqtaboutdialog.cpp" line="140"/>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="140"/>
         <source>LXQt is translated into many languages thanks to the work of the translation teams all over the world.</source>
         <comment>About dialog, &apos;Translations&apos; tab text</comment>
         <translation>LXQt перекладено на багато мов завдяки праці команд перекладачів по всьому світу.</translation>
-    </message>
-    <message>
-        <source>Yes</source>
-        <translation type="obsolete">Так</translation>
-    </message>
-    <message>
-        <source>No</source>
-        <translation type="obsolete">Ні</translation>
-    </message>
-    <message>
-        <source>&lt;b&gt;LXQt Desktop Toolbox - Technical Info&lt;/b&gt;</source>
-        <translation type="obsolete">&lt;b&gt;Інструменти стільниці LXQt - Технічна інформація&lt;/b&gt;</translation>
-    </message>
-    <message>
-        <source>Version</source>
-        <translation type="obsolete">Версія</translation>
-    </message>
-    <message>
-        <source>Qt</source>
-        <translation type="obsolete">Qt</translation>
-    </message>
-    <message>
-        <source>Debug Build</source>
-        <translation type="obsolete">Відлагоджувальна збірка</translation>
-    </message>
-    <message>
-        <source>System Configuration</source>
-        <translation type="obsolete">Сістемні налаштування</translation>
-    </message>
-    <message>
-        <source>Share Directory</source>
-        <translation type="obsolete">Спільна тека</translation>
-    </message>
-    <message>
-        <source>Translations</source>
-        <comment>About dialog, Tab title</comment>
-        <translation type="obsolete">Переклади</translation>
-    </message>
-    <message>
-        <source>&lt;b&gt;User Directories&lt;/b&gt;</source>
-        <translation type="obsolete">&lt;b&gt;Теки користувача&lt;/b&gt;</translation>
-    </message>
-    <message>
-        <source>Xdg Data Home</source>
-        <translation type="obsolete">Домівка даних Xdg</translation>
-    </message>
-    <message>
-        <source>Xdg Config Home</source>
-        <translation type="obsolete">Домівка налаштувань Xdg</translation>
-    </message>
-    <message>
-        <source>Xdg Data Dirs</source>
-        <translation type="obsolete">Текі даних Xdg</translation>
-    </message>
-    <message>
-        <source>Xdg Cache Home</source>
-        <translation type="obsolete">Домівка кешу Xdg</translation>
-    </message>
-    <message>
-        <source>Xdg Runtime Home</source>
-        <translation type="obsolete">Домівка часу виконання Xdg</translation>
-    </message>
-    <message>
-        <source>Xdg Autostart Dirs</source>
-        <translation type="obsolete">Текі автозапуску Xdg</translation>
-    </message>
-    <message>
-        <source>Xdg Autostart Home</source>
-        <translation type="obsolete">Домівка автозапуску Xdg</translation>
     </message>
 </context>
 <context>
     <name>about</name>
     <message>
-        <location filename="../../aboutdialog/lxqtaboutdialog.ui" line="14"/>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="14"/>
         <source> About LXQt</source>
         <translation> Про LXQt</translation>
     </message>
     <message>
-        <location filename="../../aboutdialog/lxqtaboutdialog.ui" line="66"/>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="66"/>
         <source>About</source>
         <comment>About dialog, Tab title </comment>
         <translation>Про</translation>
     </message>
     <message>
-        <location filename="../../aboutdialog/lxqtaboutdialog.ui" line="83"/>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="83"/>
         <source>Authors</source>
         <comment>About dialog, Tab title</comment>
         <translation>Автори</translation>
     </message>
     <message>
-        <location filename="../../aboutdialog/lxqtaboutdialog.ui" line="100"/>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="100"/>
         <source>Thanks</source>
         <comment>About dialog, Tab title</comment>
         <translation>Подяка</translation>
     </message>
     <message>
-        <location filename="../../aboutdialog/lxqtaboutdialog.ui" line="117"/>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="117"/>
         <source>Translations</source>
         <comment>About dialog, Tab title</comment>
         <translation>Перекладачі</translation>
     </message>
     <message>
-        <location filename="../../aboutdialog/lxqtaboutdialog.ui" line="134"/>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="134"/>
         <source>Technical Info</source>
         <comment>About dialog, Tab title</comment>
         <translation>Технічна інформація</translation>
     </message>
     <message>
-        <location filename="../../aboutdialog/lxqtaboutdialog.ui" line="165"/>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="165"/>
         <source>Copy to clipboard</source>
         <translation>Скопіювати в буфер обміну </translation>
     </message>
