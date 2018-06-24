3f7244f01fce7722409ba9a0e78fd284bc65167a
Oliver Burkardt <oliver.burkardt@gmail.com>
2018-06-24 22:07:21 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (17 of 17 strings)
    
    Translation: LXQt/LXQt About
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-about/de/

--- translations/lxqt-about_de.ts
+++ translations/lxqt-about_de.ts
@@ -12,7 +12,7 @@
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="96"/>
         <source>Advanced, easy-to-use, and fast desktop environment based on Qt technologies.</source>
         <comment>About dialog, &apos;About&apos; tab text</comment>
-        <translation>Fortgeschrittene, leicht zu bedienende und schnelle Arbeitsumgebung, basierend auf Qt-Technologien.</translation>
+        <translation>Fortgeschrittene, leicht zu bedienende und schnelle Desktopumgebung, basierend auf Qt-Technologien.</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="98"/>
