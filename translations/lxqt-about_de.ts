64791c8ad743339176ff14867956033c989d1ed1
Oliver Burkardt <oliver.burkardt@gmail.com>
2018-06-28 14:46:02 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (17 of 17 strings)
    
    Translation: LXQt/LXQt About
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-about/de/

--- translations/lxqt-about_de.ts
+++ translations/lxqt-about_de.ts
@@ -24,7 +24,7 @@
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="100"/>
         <source>Copyright: © %1-%2 %3</source>
         <comment>About dialog, &apos;About&apos; tab text</comment>
-        <translation>Copyright: © %1-%2 %3</translation>
+        <translation>Urheberrecht: © %1-%2 %3</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="102"/>
