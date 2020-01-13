db196a3ea2880c6c9fdd08e693f50887b896b7e4
Ács Zoltán <acszoltan111@gmail.com>
2020-01-13 15:35:45 +0000

    Translated using Weblate (Hungarian)
    Currently translated at 100.0% (17 of 17 strings)
    
    Translation: LXQt/LXQt About
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-about/hu/

--- translations/lxqt-about_hu.ts
+++ translations/lxqt-about_hu.ts
@@ -12,7 +12,7 @@
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="96"/>
         <source>Advanced, easy-to-use, and fast desktop environment based on Qt technologies.</source>
         <comment>About dialog, &apos;About&apos; tab text</comment>
-        <translation>Qt alapú könnyen használható, fejlett és gyors asztalkezelő.</translation>
+        <translation>Qt alapú könnyen használható, fejlett és gyors asztali környezet.</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="98"/>
