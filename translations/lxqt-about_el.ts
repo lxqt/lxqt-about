cf25ab87f1fd1f929a62dd56895b25383f4789a2
Dimitrios Glentadakis <dglent@free.fr>
2020-11-02 20:06:09 +0100

    Weblate commits
    Co-authored-by: Dimitrios Glentadakis <dglent@free.fr>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about/el/
    Translation: LXQt Desktop/lxqt-about

--- translations/lxqt-about_el.ts
+++ translations/lxqt-about_el.ts
@@ -60,7 +60,7 @@
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="140"/>
         <source>LXQt is translated into many languages thanks to the work of the translation teams all over the world. Translations can be done in &lt;a %1&gt;LXQt-Weblate&lt;/a&gt;.</source>
         <comment>About dialog, &apos;Translations&apos; tab text</comment>
-        <translation type="unfinished"></translation>
+        <translation>Το LXQt είναι μεταφρασμένο σε πολλές γλώσσες χάρις στις μεταφραστικές ομάδες σε ολόκληρό τον κόσμο. Οι μεταφράσεις γίνονται στο &lt;a %1&gt;LXQt-Weblate&lt;/a&gt;.</translation>
     </message>
 </context>
 <context>
