a25da7749d4e6bf3d141b1fc3ef889454507f36a
Omer I.S <omeritzicschwartz@gmail.com>
2020-11-09 08:06:17 +0100

    Weblate commits
    Co-authored-by: Omer I.S <omeritzicschwartz@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about/he/
    Translation: LXQt Desktop/lxqt-about

--- translations/lxqt-about_he.ts
+++ translations/lxqt-about_he.ts
@@ -42,7 +42,7 @@
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="114"/>
         <source>LXQt is developed by the &lt;a %1&gt;LXQt Team and contributors&lt;/a&gt;.</source>
         <comment>About dialog, &apos;Authors&apos; tab text</comment>
-        <translation>‏LXQt מפותחת על ידי &lt;a %1&gt;צוות LXQt ותורמים נוספים&lt;/a&gt;.</translation>
+        <translation>LXQt מפותחת על ידי &lt;a %1&gt;צוות LXQt ותורמים נוספים&lt;/a&gt;.</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="116"/>
