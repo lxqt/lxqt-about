a957e8645e68dac88b9e83933423eb3330cd87e9
Daniel Fischer <fischer049@gmail.com>
2020-11-19 19:06:16 +0100

    Weblate commits
    Co-authored-by: Daniel Fischer <fischer049@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about/de/
    Translation: LXQt Desktop/lxqt-about

--- translations/lxqt-about_de.ts
+++ translations/lxqt-about_de.ts
@@ -60,7 +60,7 @@
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="140"/>
         <source>LXQt is translated into many languages thanks to the work of the translation teams all over the world. Translations can be done in &lt;a %1&gt;LXQt-Weblate&lt;/a&gt;.</source>
         <comment>About dialog, &apos;Translations&apos; tab text</comment>
-        <translation type="unfinished"></translation>
+        <translation>LXQT wird in viele Sprachen übersetzt, dank der Arbeit von Übersetzungsteams auf der ganzen Welt. Die Übersetzung kann auf &lt;a %1&gt;LXQt-Weblate&lt;/a&gt; unterstützt werden.</translation>
     </message>
 </context>
 <context>
