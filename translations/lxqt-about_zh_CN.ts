ca7d17fa1e036522190428a6f09aee5dc76fb577
Kaijie Chen <chen@kaijie.org>
2020-12-06 17:42:54 +0100

    Weblate commits
    Co-authored-by: Kaijie Chen <chen@kaijie.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about/zh_Hans/
    Translation: LXQt Desktop/LXQt-about

--- translations/lxqt-about_zh_CN.ts
+++ translations/lxqt-about_zh_CN.ts
@@ -60,7 +60,7 @@
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="140"/>
         <source>LXQt is translated into many languages thanks to the work of the translation teams all over the world. Translations can be done in &lt;a %1&gt;LXQt-Weblate&lt;/a&gt;.</source>
         <comment>About dialog, &apos;Translations&apos; tab text</comment>
-        <translation type="unfinished"></translation>
+        <translation>LXQt 已被翻译成多种语言，感谢世界各地的翻译团队所做的工作。翻译可以在 &lt;a %1&gt;LXQt-Weblate&lt;/a&gt; 中进行。</translation>
     </message>
 </context>
 <context>
