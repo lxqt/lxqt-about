ce815b546ac1e228fd846b226838427ec0c9db4f
Mincho Kondarev <mkondarev@yahoo.de>
2020-12-07 18:06:23 +0100

    Weblate commits
    Co-authored-by: Mincho Kondarev <mkondarev@yahoo.de>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about-menu-entry/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about/bg/
    Translation: LXQt Desktop/LXQt-about
    Translation: LXQt Desktop/LXQt-about Menu Entry

--- translations/lxqt-about_bg.ts
+++ translations/lxqt-about_bg.ts
@@ -12,13 +12,13 @@
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="96"/>
         <source>Advanced, easy-to-use, and fast desktop environment based on Qt technologies.</source>
         <comment>About dialog, &apos;About&apos; tab text</comment>
-        <translation>Напреднала, лесна за използване и бърза работна среда, базирана на Qt технологии.</translation>
+        <translation>Разширена, лесна за използване и бърза работна среда, базирана на Qt технологии.</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="98"/>
         <source>LXQt would not have been possible without the &lt;a %1&gt;Razor-qt&lt;/a&gt; project and its many contributors.</source>
         <comment>About dialog, &apos;About&apos; tab text</comment>
-        <translation>LXQt не би било възможно без проекта &lt;a %1&gt; Razor-qt &lt;/a&gt; и многото му сътрудници.</translation>
+        <translation>LXQt нямаше да бъде възможен без &lt;a %1&gt;Razor-qt&lt;/a&gt; проекта и неговите многобройни сътрудници.</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="100"/>
@@ -42,25 +42,25 @@
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="114"/>
         <source>LXQt is developed by the &lt;a %1&gt;LXQt Team and contributors&lt;/a&gt;.</source>
         <comment>About dialog, &apos;Authors&apos; tab text</comment>
-        <translation>LXQt е разработен от &lt;a %1&gt; LXQt екип и сътрудници &lt;/a&gt;.</translation>
+        <translation>LXQt е разработен от екипа на &lt;a %1&gt;LXQt и сътрудници &lt;/a&gt;.</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="116"/>
         <source>If you are interested in working with our development team, &lt;a %1&gt;join us&lt;/a&gt;.</source>
         <comment>About dialog, &apos;Authors&apos; tab text</comment>
-        <translation>Ако се интересувате от работата с екипа ни за разработка, &lt;a href=&quot;&quot;&gt; се присъединете към нас &lt;/a&gt;.</translation>
+        <translation>Ако се интересувате от работа с нашия екип за разработка, &lt;a %1&gt;присъединете се тук&lt;/a&gt;.</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="133"/>
         <source>Special thanks to:</source>
         <comment>About dialog, &apos;Thanks&apos; tab text</comment>
-        <translation>Специални благодарности на:</translation>
+        <translation>Специални благодарности:</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="140"/>
         <source>LXQt is translated into many languages thanks to the work of the translation teams all over the world. Translations can be done in &lt;a %1&gt;LXQt-Weblate&lt;/a&gt;.</source>
         <comment>About dialog, &apos;Translations&apos; tab text</comment>
-        <translation>LXQt се превежда на много езици благодарение на работата на преводаческите екипи по целия свят. Преводи могат да се извършват на  &lt;a %1&gt;LXQt-Weblate&lt;/a&gt;.</translation>
+        <translation>LXQt се превежда на много езици благодарение на работата на преводаческите екипи по целия свят. Преводи могат да се извършват на &lt;a %1&gt;LXQt-Weblate&lt;/a&gt;.</translation>
     </message>
 </context>
 <context>
@@ -69,13 +69,13 @@
         <location filename="../aboutdialog/lxqtaboutdialog.ui" line="165"/>
         <location filename="../build/lxqt-about_autogen/include/ui_lxqtaboutdialog.h" line="194"/>
         <source>Copy to clipboard</source>
-        <translation>Копиране в клипборда</translation>
+        <translation>Копиране в обменния буфер</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.ui" line="14"/>
         <location filename="../build/lxqt-about_autogen/include/ui_lxqtaboutdialog.h" line="189"/>
         <source>About LXQt</source>
-        <translation>За LXQt</translation>
+        <translation>Относно LXQt</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.ui" line="66"/>
@@ -96,7 +96,7 @@
         <location filename="../build/lxqt-about_autogen/include/ui_lxqtaboutdialog.h" line="192"/>
         <source>Thanks</source>
         <comment>About dialog, Tab title</comment>
-        <translation>Благодаря</translation>
+        <translation>Благодарности</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.ui" line="117"/>
