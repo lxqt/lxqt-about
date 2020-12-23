f010489be98657b083fafb593745d7973f1750f9
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2020-12-23 08:08:53 +0100

    Translations update from Weblate (#172)
    * Weblate commits
    
    Co-authored-by: scootergrisen <scootergrisen@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about/da/
    Translation: LXQt Desktop/LXQt-about
    
    * Weblate commits
    
    Co-authored-by: milotype <mail@milotype.de>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about/hr/
    Translation: LXQt Desktop/LXQt-about
    
    Co-authored-by: scootergrisen <scootergrisen@gmail.com>
    Co-authored-by: milotype <mail@milotype.de>

--- translations/lxqt-about_hr.ts
+++ translations/lxqt-about_hr.ts
@@ -60,7 +60,7 @@
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="140"/>
         <source>LXQt is translated into many languages thanks to the work of the translation teams all over the world. Translations can be done in &lt;a %1&gt;LXQt-Weblate&lt;/a&gt;.</source>
         <comment>About dialog, &apos;Translations&apos; tab text</comment>
-        <translation type="unfinished"></translation>
+        <translation>LXQt je preveden na mnoge jezike zahvaljujući radu prevoditeljskih timova širom svijeta. Prevodi se na &lt;a %1&gt;LXQt-Weblate&lt;/a&gt;.</translation>
     </message>
 </context>
 <context>
