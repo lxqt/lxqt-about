e9c2b6468e01bf237942e1b6cf801bdd31a16c2a
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-01-12 17:59:13 +0100

    Translations update from Weblate (#176)
    * Weblate commits
    
    Co-authored-by: Golubev Alexander <fatzer2@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about-menu-entry/ru/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about/ru/
    Translation: LXQt Desktop/LXQt-about
    Translation: LXQt Desktop/LXQt-about Menu Entry
    
    * Weblate commits
    
    Co-authored-by: jetomit <timotej.lazar@araneo.si>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about/sl/
    Translation: LXQt Desktop/LXQt-about
    
    Co-authored-by: Golubev Alexander <fatzer2@gmail.com>
    Co-authored-by: jetomit <timotej.lazar@araneo.si>

--- translations/lxqt-about_ru.ts
+++ translations/lxqt-about_ru.ts
@@ -60,7 +60,7 @@
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="140"/>
         <source>LXQt is translated into many languages thanks to the work of the translation teams all over the world. Translations can be done in &lt;a %1&gt;LXQt-Weblate&lt;/a&gt;.</source>
         <comment>About dialog, &apos;Translations&apos; tab text</comment>
-        <translation type="unfinished"></translation>
+        <translation>LXQt переведён на множество языков благодаря работе команд переводчиков по всему миру. Присоединиться к переводу можно на сайте &lt;a %1&gt;LXQt-Weblate&lt;/a&gt;.</translation>
     </message>
 </context>
 <context>
