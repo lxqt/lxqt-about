ad7b3868d794a1c823fb9b320556d167140eb37e
Quentin PAGÈS <quentinantonin@free.fr>
2021-02-20 21:06:50 +0100

    Weblate commits
    Co-authored-by: Quentin PAGÈS <quentinantonin@free.fr>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about/oc/
    Translation: LXQt Desktop/LXQt-about

---
+++ translations/lxqt-about_oc.ts
@@ -0,0 +1,116 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="oc">
+<context>
+    <name>AboutDialogPrivate</name>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="83"/>
+        <source>Version: %1</source>
+        <translation>Version : %1</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="96"/>
+        <source>Advanced, easy-to-use, and fast desktop environment based on Qt technologies.</source>
+        <comment>About dialog, &apos;About&apos; tab text</comment>
+        <translation>Environament de burèu avançat, facil d’utilizar e rapid basat sus la tecnologia Qt.</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="98"/>
+        <source>LXQt would not have been possible without the &lt;a %1&gt;Razor-qt&lt;/a&gt; project and its many contributors.</source>
+        <comment>About dialog, &apos;About&apos; tab text</comment>
+        <translation>LXQt seriá pas estat possible sens lo projècte &lt;a %1&gt;Razor-qt&lt;/a&gt; e sos nombroses contributors.</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="100"/>
+        <source>Copyright: © %1-%2 %3</source>
+        <comment>About dialog, &apos;About&apos; tab text</comment>
+        <translation>Copyright : © %1-%2 %3</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="102"/>
+        <source>Homepage: %1</source>
+        <comment>About dialog, &apos;About&apos; tab text</comment>
+        <translation>Pagina web : %1</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="104"/>
+        <source>License: %1</source>
+        <comment>About dialog, &apos;About&apos; tab text</comment>
+        <translation>Licéncia : %1</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="114"/>
+        <source>LXQt is developed by the &lt;a %1&gt;LXQt Team and contributors&lt;/a&gt;.</source>
+        <comment>About dialog, &apos;Authors&apos; tab text</comment>
+        <translation>LXQt es desvelopat per &lt;a %1&gt;l’equipa LXQt e los contributors&lt;/a&gt;.</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="116"/>
+        <source>If you are interested in working with our development team, &lt;a %1&gt;join us&lt;/a&gt;.</source>
+        <comment>About dialog, &apos;Authors&apos; tab text</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="133"/>
+        <source>Special thanks to:</source>
+        <comment>About dialog, &apos;Thanks&apos; tab text</comment>
+        <translation>Mercejaments particular a :</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="140"/>
+        <source>LXQt is translated into many languages thanks to the work of the translation teams all over the world. Translations can be done in &lt;a %1&gt;LXQt-Weblate&lt;/a&gt;.</source>
+        <comment>About dialog, &apos;Translations&apos; tab text</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>about</name>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="165"/>
+        <location filename="../build/lxqt-about_autogen/include/ui_lxqtaboutdialog.h" line="194"/>
+        <source>Copy to clipboard</source>
+        <translation>Copiar al quichapapièrs</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="14"/>
+        <location filename="../build/lxqt-about_autogen/include/ui_lxqtaboutdialog.h" line="189"/>
+        <source>About LXQt</source>
+        <translation>A prepaus de LXQt</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="66"/>
+        <location filename="../build/lxqt-about_autogen/include/ui_lxqtaboutdialog.h" line="190"/>
+        <source>About</source>
+        <comment>About dialog, Tab title </comment>
+        <translation>A prepaus</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="83"/>
+        <location filename="../build/lxqt-about_autogen/include/ui_lxqtaboutdialog.h" line="191"/>
+        <source>Authors</source>
+        <comment>About dialog, Tab title</comment>
+        <translation>Autors</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="100"/>
+        <location filename="../build/lxqt-about_autogen/include/ui_lxqtaboutdialog.h" line="192"/>
+        <source>Thanks</source>
+        <comment>About dialog, Tab title</comment>
+        <translation>Mercés</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="117"/>
+        <location filename="../build/lxqt-about_autogen/include/ui_lxqtaboutdialog.h" line="193"/>
+        <source>Translations</source>
+        <comment>About dialog, Tab title</comment>
+        <translation>Traduccions</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="134"/>
+        <location filename="../build/lxqt-about_autogen/include/ui_lxqtaboutdialog.h" line="195"/>
+        <source>Technical Info</source>
+        <comment>About dialog, Tab title</comment>
+        <translation>Info tecnicas</translation>
+    </message>
+</context>
+</TS>
