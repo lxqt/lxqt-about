ac488728fd2619c40e2f2479204a4155169af94e
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-03-16 21:15:26 +0100

    Translations update from Weblate (#185)
    * Weblate commits
    
    Co-authored-by: helabasa <R45XvezA@pm.me>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about/si/
    Translation: LXQt Desktop/LXQt-about
    
    * Weblate commits
    
    Co-authored-by: Allan Nordhøy <epost@anotheragency.no>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about/nb_NO/
    Translation: LXQt Desktop/LXQt-about
    
    * Weblate commits
    
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about-menu-entry/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about/et/
    Translation: LXQt Desktop/LXQt-about
    Translation: LXQt Desktop/LXQt-about Menu Entry
    
    Co-authored-by: helabasa <R45XvezA@pm.me>
    Co-authored-by: Allan Nordhøy <epost@anotheragency.no>
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>

--- translations/lxqt-about_si.ts
+++ translations/lxqt-about_si.ts
@@ -1,66 +1,66 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="nb_NO">
+<TS version="2.1" language="si">
 <context>
     <name>AboutDialogPrivate</name>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="83"/>
         <source>Version: %1</source>
-        <translation>Versjon: %1</translation>
+        <translation>අනුවාදය: %1</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="96"/>
         <source>Advanced, easy-to-use, and fast desktop environment based on Qt technologies.</source>
         <comment>About dialog, &apos;About&apos; tab text</comment>
-        <translation>Avansert, enkelt og raskt skrivebordsmiljø basert på Qt-teknologier.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="98"/>
         <source>LXQt would not have been possible without the &lt;a %1&gt;Razor-qt&lt;/a&gt; project and its many contributors.</source>
         <comment>About dialog, &apos;About&apos; tab text</comment>
-        <translation>LXQt hadde ikke vært mulig uten &lt;a %1&gt;Razor-qt&lt;/a&gt;-prosjektet og dets mange bidragsytere.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="100"/>
         <source>Copyright: © %1-%2 %3</source>
         <comment>About dialog, &apos;About&apos; tab text</comment>
-        <translation>Opphavsrett: © %1-%2 %3</translation>
+        <translation>ප්‍රකාශන හිමිකම: © %1-%2 %3</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="102"/>
         <source>Homepage: %1</source>
         <comment>About dialog, &apos;About&apos; tab text</comment>
-        <translation>Hjemmeside: %1</translation>
+        <translation>මුල් පිටුව: %1</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="104"/>
         <source>License: %1</source>
         <comment>About dialog, &apos;About&apos; tab text</comment>
-        <translation>Lisens: %1</translation>
+        <translation>බලපත්‍රය: %1</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="114"/>
         <source>LXQt is developed by the &lt;a %1&gt;LXQt Team and contributors&lt;/a&gt;.</source>
         <comment>About dialog, &apos;Authors&apos; tab text</comment>
-        <translation>LXQt er utviklet av &lt;a %1&gt;LXQt-gruppa og bidragsyterne&lt;/a&gt;.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="116"/>
         <source>If you are interested in working with our development team, &lt;a %1&gt;join us&lt;/a&gt;.</source>
         <comment>About dialog, &apos;Authors&apos; tab text</comment>
-        <translation>&lt;a %1&gt;Bli med&lt;/a&gt; hvis du har lyst til å jobbe med vårt utviklerteam.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="133"/>
         <source>Special thanks to:</source>
         <comment>About dialog, &apos;Thanks&apos; tab text</comment>
-        <translation>En ekstra takk til:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="140"/>
         <source>LXQt is translated into many languages thanks to the work of the translation teams all over the world. Translations can be done in &lt;a %1&gt;LXQt-Weblate&lt;/a&gt;.</source>
         <comment>About dialog, &apos;Translations&apos; tab text</comment>
-        <translation>LXQt er oversatt til mange språk takket være oversettelseslag verden over. Bistå oversettelsen på &lt;a %1&gt;LXQt-Weblate&lt;/a&gt;.</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -69,48 +69,48 @@
         <location filename="../aboutdialog/lxqtaboutdialog.ui" line="165"/>
         <location filename="../build/lxqt-about_autogen/include/ui_lxqtaboutdialog.h" line="194"/>
         <source>Copy to clipboard</source>
-        <translation>Kopier til utklippstavlen</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.ui" line="14"/>
         <location filename="../build/lxqt-about_autogen/include/ui_lxqtaboutdialog.h" line="189"/>
         <source>About LXQt</source>
-        <translation>Om LXQt</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.ui" line="66"/>
         <location filename="../build/lxqt-about_autogen/include/ui_lxqtaboutdialog.h" line="190"/>
         <source>About</source>
         <comment>About dialog, Tab title </comment>
-        <translation>Om</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.ui" line="83"/>
         <location filename="../build/lxqt-about_autogen/include/ui_lxqtaboutdialog.h" line="191"/>
         <source>Authors</source>
         <comment>About dialog, Tab title</comment>
-        <translation>Opphavsmenn</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.ui" line="100"/>
         <location filename="../build/lxqt-about_autogen/include/ui_lxqtaboutdialog.h" line="192"/>
         <source>Thanks</source>
         <comment>About dialog, Tab title</comment>
-        <translation>Takk</translation>
+        <translation>ස්තූතියි</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.ui" line="117"/>
         <location filename="../build/lxqt-about_autogen/include/ui_lxqtaboutdialog.h" line="193"/>
         <source>Translations</source>
         <comment>About dialog, Tab title</comment>
-        <translation>Oversettelser</translation>
+        <translation>පරිවර්තන</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.ui" line="134"/>
         <location filename="../build/lxqt-about_autogen/include/ui_lxqtaboutdialog.h" line="195"/>
         <source>Technical Info</source>
         <comment>About dialog, Tab title</comment>
-        <translation>Teknisk info</translation>
+        <translation>තාක්ෂණික තොරතුරු</translation>
     </message>
 </context>
 </TS>
