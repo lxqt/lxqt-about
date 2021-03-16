1b12b954b62ad48349c10dd24afe815d5d5e2c42
Priit Jõerüüt <lxqtwlate@joeruut.com>
2021-03-16 21:14:09 +0100

    Weblate commits
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about-menu-entry/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about/et/
    Translation: LXQt Desktop/LXQt-about
    Translation: LXQt Desktop/LXQt-about Menu Entry

---
+++ translations/lxqt-about_et.ts
@@ -0,0 +1,116 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="et">
+<context>
+    <name>AboutDialogPrivate</name>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="83"/>
+        <source>Version: %1</source>
+        <translation>Versioon: %1</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="96"/>
+        <source>Advanced, easy-to-use, and fast desktop environment based on Qt technologies.</source>
+        <comment>About dialog, &apos;About&apos; tab text</comment>
+        <translation>Paljude võimalustega, lihtsaltkasutatav ning kiire töölauakeskkond, mis põhineb Qt teekidel.</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="98"/>
+        <source>LXQt would not have been possible without the &lt;a %1&gt;Razor-qt&lt;/a&gt; project and its many contributors.</source>
+        <comment>About dialog, &apos;About&apos; tab text</comment>
+        <translation>Ilma kunagise &lt;a %1&gt;Razor-qt&lt;/a&gt; projekti ja tema paljude kaasautoriteta poleks LXQt teoks saanud.</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="100"/>
+        <source>Copyright: © %1-%2 %3</source>
+        <comment>About dialog, &apos;About&apos; tab text</comment>
+        <translation>Autoriõigused: © %1-%2 %3</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="102"/>
+        <source>Homepage: %1</source>
+        <comment>About dialog, &apos;About&apos; tab text</comment>
+        <translation>Veebisait: %1</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="104"/>
+        <source>License: %1</source>
+        <comment>About dialog, &apos;About&apos; tab text</comment>
+        <translation>Litsents: %1</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="114"/>
+        <source>LXQt is developed by the &lt;a %1&gt;LXQt Team and contributors&lt;/a&gt;.</source>
+        <comment>About dialog, &apos;Authors&apos; tab text</comment>
+        <translation>LXQt arenduse eest vastutab &lt;a %1&gt;LXQt tiim koos kaasautoritega&lt;/a&gt;.</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="116"/>
+        <source>If you are interested in working with our development team, &lt;a %1&gt;join us&lt;/a&gt;.</source>
+        <comment>About dialog, &apos;Authors&apos; tab text</comment>
+        <translation>Kui sulle meeldiks töötada koos meie arendajatega, siis &lt;a %1&gt;liitu meiega&lt;/a&gt;.</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="133"/>
+        <source>Special thanks to:</source>
+        <comment>About dialog, &apos;Thanks&apos; tab text</comment>
+        <translation>Eraldi soovime tänada:</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="140"/>
+        <source>LXQt is translated into many languages thanks to the work of the translation teams all over the world. Translations can be done in &lt;a %1&gt;LXQt-Weblate&lt;/a&gt;.</source>
+        <comment>About dialog, &apos;Translations&apos; tab text</comment>
+        <translation>LXQt on tõlgitud väga paljudesse keeltesse tänu meie tõlkijatest kaasautoritele üle kogu maailma. Tõlkimiseks kasutame &lt;a %1&gt;LXQt-Weblate&lt;/a&gt; veebirakendust.</translation>
+    </message>
+</context>
+<context>
+    <name>about</name>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="165"/>
+        <location filename="../build/lxqt-about_autogen/include/ui_lxqtaboutdialog.h" line="194"/>
+        <source>Copy to clipboard</source>
+        <translation>Kopeeri lõikelauale</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="14"/>
+        <location filename="../build/lxqt-about_autogen/include/ui_lxqtaboutdialog.h" line="189"/>
+        <source>About LXQt</source>
+        <translation>Teave LXQt kohta</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="66"/>
+        <location filename="../build/lxqt-about_autogen/include/ui_lxqtaboutdialog.h" line="190"/>
+        <source>About</source>
+        <comment>About dialog, Tab title </comment>
+        <translation>Üldteave</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="83"/>
+        <location filename="../build/lxqt-about_autogen/include/ui_lxqtaboutdialog.h" line="191"/>
+        <source>Authors</source>
+        <comment>About dialog, Tab title</comment>
+        <translation>Autorid</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="100"/>
+        <location filename="../build/lxqt-about_autogen/include/ui_lxqtaboutdialog.h" line="192"/>
+        <source>Thanks</source>
+        <comment>About dialog, Tab title</comment>
+        <translation>Tänud</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="117"/>
+        <location filename="../build/lxqt-about_autogen/include/ui_lxqtaboutdialog.h" line="193"/>
+        <source>Translations</source>
+        <comment>About dialog, Tab title</comment>
+        <translation>Tõlked</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="134"/>
+        <location filename="../build/lxqt-about_autogen/include/ui_lxqtaboutdialog.h" line="195"/>
+        <source>Technical Info</source>
+        <comment>About dialog, Tab title</comment>
+        <translation>Tehniline teave</translation>
+    </message>
+</context>
+</TS>
