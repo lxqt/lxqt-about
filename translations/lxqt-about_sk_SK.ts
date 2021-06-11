cb700ba2b0fdf1e064ea6fea6c8a9a391b4409f2
pavol polonec <pavolpolonec@gmail.com>
2021-06-11 20:56:26 +0200

    Weblate commits
    Co-authored-by: pavol polonec <pavolpolonec@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about-menu-entry/sk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about/sk/
    Translation: LXQt Desktop/LXQt-about
    Translation: LXQt Desktop/LXQt-about Menu Entry

--- translations/lxqt-about_sk_SK.ts
+++ translations/lxqt-about_sk_SK.ts
@@ -60,7 +60,7 @@
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="140"/>
         <source>LXQt is translated into many languages thanks to the work of the translation teams all over the world. Translations can be done in &lt;a %1&gt;LXQt-Weblate&lt;/a&gt;.</source>
         <comment>About dialog, &apos;Translations&apos; tab text</comment>
-        <translation type="unfinished"></translation>
+        <translation>Prostredie LXQt je preložené do mnohých jazykov vďaka prekladateľským tímom z celého sveta. S prekladom môžete pomôcť na &lt;a %1&gt;LXQt-Weblate&lt;/a&gt;.</translation>
     </message>
 </context>
 <context>
