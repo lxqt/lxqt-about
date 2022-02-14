04f321ec0eacb2256c77d4377cfbac803cb9b49a
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2022-02-14 10:38:48 +0100

    Weblate commits (#224)
    Co-authored-by: Dejweed <12davidkac@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about-menu-entry/sl/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about/sl/
    Translation: LXQt Desktop/LXQt-about
    Translation: LXQt Desktop/LXQt-about Menu Entry

--- translations/lxqt-about_sl.ts
+++ translations/lxqt-about_sl.ts
@@ -12,7 +12,7 @@
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="99"/>
         <source>Advanced, easy-to-use, and fast desktop environment based on Qt technologies.</source>
         <comment>About dialog, &apos;About&apos; tab text</comment>
-        <translation>Napredno, preprosto in hitro namizno okolje, osnovano na ogrodju Qt.</translation>
+        <translation>Napredno, preprosto in hitro namizno okolje, zasnovano na ogrodju Qt.</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="101"/>
@@ -36,13 +36,13 @@
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="107"/>
         <source>Wiki: %1</source>
         <comment>About dialog, &apos;About&apos; tab text</comment>
-        <translation type="unfinished"></translation>
+        <translation>Wiki: %1</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="109"/>
         <source>Development: %1</source>
         <comment>About dialog, &apos;About&apos; tab text</comment>
-        <translation type="unfinished"></translation>
+        <translation>Razvoj: %1</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="111"/>
