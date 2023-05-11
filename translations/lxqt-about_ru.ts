136799fa6dc731764ed72fc1d3b81e63a74b9655
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2023-05-11 18:42:36 +0200

    Weblate commits (#257)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about-menu-entry/fi/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about-menu-entry/ru/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about/ru/
    Translation: LXQt Desktop/LXQt-about
    Translation: LXQt Desktop/LXQt-about Menu Entry
    
    Co-authored-by: Andrei Stepanov <adem4ik@gmail.com>
    Co-authored-by: Jouni Järvinen <jounijarvis@gmail.com>
    Co-authored-by: antohami <antohami@altlinux.org>

--- translations/lxqt-about_ru.ts
+++ translations/lxqt-about_ru.ts
@@ -12,7 +12,7 @@
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="104"/>
         <source>Advanced, easy-to-use, and fast desktop environment based on Qt technologies.</source>
         <comment>About dialog, &apos;About&apos; tab text</comment>
-        <translation>Передовая, быстрая, и простая в использовании среда рабочего стола на основе технологий Qt.</translation>
+        <translation>Передовая, быстрая и простая в использовании среда рабочего стола на основе технологий Qt.</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="106"/>
