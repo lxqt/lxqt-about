9313fb2d186725bb8b1105ab1b38404d541c6902
Weblate <noreply@weblate.org>
2023-12-26 01:45:59 +0100

    Weblate commits
    Co-authored-by: A S Alam <amanpreet.alam@gmail.com>
    Co-authored-by: Sensalire <kbirabwa@yahoo.co.uk>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about-menu-entry/pa/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about/lg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about/pa/
    Translation: LXQt Desktop/LXQt-about
    Translation: LXQt Desktop/LXQt-about Menu Entry

---
+++ translations/lxqt-about_lg.ts
@@ -0,0 +1,121 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lg">
+<context>
+    <name>AboutDialogPrivate</name>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="88"/>
+        <source>Version: %1</source>
+        <translation>Luwandika: %1</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="104"/>
+        <source>Advanced, easy-to-use, and fast desktop environment based on Qt technologies.</source>
+        <comment>About dialog, &apos;About&apos; tab text</comment>
+        <translation>Ekiteekateeka awakolerwa eky&apos;omulembe, era ekikola mangu ekyesigamizidwa tekinologiya wa Qt.</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="106"/>
+        <source>LXQt would not have been possible without the &lt;a %1&gt;Razor-qt&lt;/a&gt; project and its many contributors.</source>
+        <comment>About dialog, &apos;About&apos; tab text</comment>
+        <translation>Tekyandisobose okukola LXQt singa tewaaliwo kawefube wa &lt;a %1&gt;Razor-qt&lt;/a&gt; n&apos;abaamwenyigiramu bonna.</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="108"/>
+        <source>Copyright: © %1-%2 %3</source>
+        <comment>About dialog, &apos;About&apos; tab text</comment>
+        <translation>Obuyinza bw&apos;ebikugizo eby&apos;obwa nannyini: © %1-%2 %3</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="110"/>
+        <source>Homepage: %1</source>
+        <comment>About dialog, &apos;About&apos; tab text</comment>
+        <translation>Obusangiro ku mutimbagano: %1</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="112"/>
+        <source>Wiki: %1</source>
+        <comment>About dialog, &apos;About&apos; tab text</comment>
+        <translation>Wiki: %1</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="114"/>
+        <source>Development: %1</source>
+        <comment>About dialog, &apos;About&apos; tab text</comment>
+        <translation>Eby&apos;okwenyigira mu kuwandika sofutiweya ono: %1</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="116"/>
+        <source>License: %1</source>
+        <comment>About dialog, &apos;About&apos; tab text</comment>
+        <translation>Layisensi: %1</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="126"/>
+        <source>LXQt is developed by the &lt;a %1&gt;LXQt Team and contributors&lt;/a&gt;.</source>
+        <comment>About dialog, &apos;Authors&apos; tab text</comment>
+        <translation>LXQt ewandikibwa &lt;a %1&gt;tiimu eya LXQt n&apos;ababayamba&lt;/a&gt;.</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="128"/>
+        <source>If you are interested in working with our development team, &lt;a %1&gt;join us&lt;/a&gt;.</source>
+        <comment>About dialog, &apos;Authors&apos; tab text</comment>
+        <translation>Oba oyagala okukola ne tiimu yaffe ewandika sofutiweya ono, &lt;a %1&gt;jangu otwegatteko&lt;/a&gt;.</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="145"/>
+        <source>Special thanks to:</source>
+        <comment>About dialog, &apos;Thanks&apos; tab text</comment>
+        <translation>Twebaliza ddala:</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="152"/>
+        <source>LXQt is translated into many languages thanks to the work of the translation teams all over the world. Translations can be done in &lt;a %1&gt;LXQt-Weblate&lt;/a&gt;.</source>
+        <comment>About dialog, &apos;Translations&apos; tab text</comment>
+        <translation>Twebaza tiimu ezibunye nsi yonna ezikyusizza LXQt ekolere mu nnimi nkumu. Okwenyigira mu kukyusa kuno genda ku &lt;a %1&gt;LXQt-Weblate&lt;/a&gt;.</translation>
+    </message>
+</context>
+<context>
+    <name>about</name>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="165"/>
+        <source>Copy to clipboard</source>
+        <translation>Teeka mu ggwanika ly&apos;ebikoppedwa</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="14"/>
+        <source>About LXQt</source>
+        <translation>Ebifa ku LXQt</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="66"/>
+        <source>About</source>
+        <comment>About dialog, Tab title </comment>
+        <translation>Okumanyisa</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="83"/>
+        <source>Authors</source>
+        <comment>About dialog, Tab title</comment>
+        <translation>Abawandisi</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="100"/>
+        <source>Thanks</source>
+        <comment>About dialog, Tab title</comment>
+        <translation>Okwebaza</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="117"/>
+        <source>Translations</source>
+        <comment>About dialog, Tab title</comment>
+        <translation>Abaakyusa</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="134"/>
+        <source>Technical Info</source>
+        <comment>About dialog, Tab title</comment>
+        <translation>Eby&apos;enkola ya sofutiweya</translation>
+    </message>
+</context>
+</TS>
