c6389b1085c57d9554e66cd6068b1782e96c436d
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2023-12-26 08:37:10 +0100

    Weblate commits (#266)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about-menu-entry/pa/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about/lg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about/pa/
    Translation: LXQt Desktop/LXQt-about
    Translation: LXQt Desktop/LXQt-about Menu Entry
    
    Co-authored-by: A S Alam <amanpreet.alam@gmail.com>
    Co-authored-by: Sensalire <kbirabwa@yahoo.co.uk>

---
+++ translations/lxqt-about_pa.ts
@@ -0,0 +1,121 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pa">
+<context>
+    <name>AboutDialogPrivate</name>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="88"/>
+        <source>Version: %1</source>
+        <translation>ਵਰਜ਼ਨ: %1</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="104"/>
+        <source>Advanced, easy-to-use, and fast desktop environment based on Qt technologies.</source>
+        <comment>About dialog, &apos;About&apos; tab text</comment>
+        <translation>Qt ਤਕਨੀਕਾਂ ਉੱਤੇ ਅਧਾਰਿਤ ਅਗਾਂਹਵਧੂ, ਵਰਤਣ ਲਈ ਸੌਖਾ,  ਤੇਜ਼ ਡੈਸਕਟਾਪ ਇਨਵਾਇਰਮੈਂਟ ਹੈ।</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="106"/>
+        <source>LXQt would not have been possible without the &lt;a %1&gt;Razor-qt&lt;/a&gt; project and its many contributors.</source>
+        <comment>About dialog, &apos;About&apos; tab text</comment>
+        <translation>&lt;a %1&gt;Razor-qt&lt;/a&gt; ਪਰੋਜੈਕਟ ਅਤੇ ਇਸ ਦੇ ਯੋਗਦਾਨੀਆਂ ਬਿਨਾਂ LXQt ਸੰਭਵ ਨਹੀਂ ਹੋ ਸਕਣਾ ਸੀ।</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="108"/>
+        <source>Copyright: © %1-%2 %3</source>
+        <comment>About dialog, &apos;About&apos; tab text</comment>
+        <translation>ਕਾਪੀਰਾਈਟ: © %1-%2 %3</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="110"/>
+        <source>Homepage: %1</source>
+        <comment>About dialog, &apos;About&apos; tab text</comment>
+        <translation>ਮੁੱਖ ਸਫ਼ਾ: %1</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="112"/>
+        <source>Wiki: %1</source>
+        <comment>About dialog, &apos;About&apos; tab text</comment>
+        <translation>ਵਿਕੀ: %1</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="114"/>
+        <source>Development: %1</source>
+        <comment>About dialog, &apos;About&apos; tab text</comment>
+        <translation>ਡਿਵੈਲਪਮੈਂਟ: %1</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="116"/>
+        <source>License: %1</source>
+        <comment>About dialog, &apos;About&apos; tab text</comment>
+        <translation>ਲਸੰਸ: %1</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="126"/>
+        <source>LXQt is developed by the &lt;a %1&gt;LXQt Team and contributors&lt;/a&gt;.</source>
+        <comment>About dialog, &apos;Authors&apos; tab text</comment>
+        <translation>LXQt ਨੂੰ &lt;a %1&gt;LXQt ਟੀਮ ਅਤੇ ਯੋਗਦਾਨ ਪਾਉਣ ਵਾਲਿਆਂ&lt;/a&gt; ਵਲੋਂ ਡਿਵੈਲਪ ਕੀਤਾ ਗਿਆ ਹੈ।</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="128"/>
+        <source>If you are interested in working with our development team, &lt;a %1&gt;join us&lt;/a&gt;.</source>
+        <comment>About dialog, &apos;Authors&apos; tab text</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="145"/>
+        <source>Special thanks to:</source>
+        <comment>About dialog, &apos;Thanks&apos; tab text</comment>
+        <translation>ਖਾਸ ਧੰਨਵਾਦ ਹੈ:</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="152"/>
+        <source>LXQt is translated into many languages thanks to the work of the translation teams all over the world. Translations can be done in &lt;a %1&gt;LXQt-Weblate&lt;/a&gt;.</source>
+        <comment>About dialog, &apos;Translations&apos; tab text</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>about</name>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="165"/>
+        <source>Copy to clipboard</source>
+        <translation>ਕਲਿੱਪਬੋਰਡ ਵਿੱਚ ਕਾਪੀ ਕਰੋ</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="14"/>
+        <source>About LXQt</source>
+        <translation>LXQt ਬਾਰੇ</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="66"/>
+        <source>About</source>
+        <comment>About dialog, Tab title </comment>
+        <translation>ਇਸ ਬਾਰੇ</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="83"/>
+        <source>Authors</source>
+        <comment>About dialog, Tab title</comment>
+        <translation>ਲੇਖਕ</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="100"/>
+        <source>Thanks</source>
+        <comment>About dialog, Tab title</comment>
+        <translation>ਧੰਨਵਾਦ</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="117"/>
+        <source>Translations</source>
+        <comment>About dialog, Tab title</comment>
+        <translation>ਉਲੱਥਾਕਾਰ</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog/lxqtaboutdialog.ui" line="134"/>
+        <source>Technical Info</source>
+        <comment>About dialog, Tab title</comment>
+        <translation>ਤਕਨੀਕੀ ਜਾਣਕਾਰੀ</translation>
+    </message>
+</context>
+</TS>
