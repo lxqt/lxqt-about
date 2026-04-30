76e454db39c1e5b51838a28128826437f3edf2c6
Weblate <noreply@weblate.org>
2026-04-30 09:28:50 +0200

    Weblate commits
    Co-authored-by: Fabio <fabiocan@live.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-about/eo/
    Translation: LXQt Desktop/LXQt-about

--- translations/lxqt-about_eo.ts
+++ translations/lxqt-about_eo.ts
@@ -72,7 +72,7 @@
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="152"/>
         <source>LXQt is translated into many languages thanks to the work of the translation teams all over the world. Translations can be done in &lt;a %1&gt;LXQt-Weblate&lt;/a&gt;.</source>
         <comment>About dialog, &apos;Translations&apos; tab text</comment>
-        <translation type="unfinished"></translation>
+        <translation>LXQt estas tradukita en muktaj lingvoj danke al la laboro de tradukaj teamoj en la tuta mondo. Oni povas traduki ĉe &lt;a %1&gt;LXQt-Weblate&lt;/a&gt;.</translation>
     </message>
 </context>
 <context>
